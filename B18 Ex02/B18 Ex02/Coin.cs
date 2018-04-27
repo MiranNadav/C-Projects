fae01066216218a07b9f005eec956419df927db5
David Shemian <DavidShemian18@gmail.com>
2018-04-27 16:39:44 +0300

    new coin

--- B18 Ex02/B18 Ex02/Coin.cs
+++ B18 Ex02/B18 Ex02/Coin.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks; 
+using System.Threading.Tasks;
 
 namespace B18_Ex02
 {
@@ -10,18 +10,23 @@ namespace B18_Ex02
     {
         private char m_Row;
         private char m_Column;
+        private string currentSquare;
         private char m_Type;
+        private bool m_isKing;
+
+
         private enum coinType
         {
-            X,O
+            X, O
         }
-        
 
-        public Coin (char i_Row, char i_Column, char i_Type)
+        public Coin(char i_Row, char i_Column, char i_Type)
         {
             this.m_Row = i_Row;
             this.m_Column = i_Column;
+            this.currentSquare = String.Empty + i_Column + i_Row;
             this.m_Type = i_Type;
+            this.m_isKing = false;
         }
 
         public Coin(int i_Row, int i_Column, char i_Type)
@@ -31,41 +36,8 @@ namespace B18_Ex02
             this.m_Type = i_Type;
         }
 
-        
-        private void parseRowLocation (int i_indexOfRow)
-        {
-            this.m_Row = (char)(i_indexOfRow + 49);
-            //switch (i_indexOfRow)
-            //{
-            //    case 0:
-            //        this.Row = 'a';
-            //        break;
-            //    case 1:
-            //        this.Row = 'b';
-            //        break;
-            //    case 2:
-            //        this.Row = 'c';
-            //        break;
-            //    case 3:
-            //        this.Row = 'd';
-            //        break;
-            //    case 4:
-            //        this.Row = 'e';
-            //        break;
-            //    case 5:
-            //        this.Row = 'f';
-            //        break;
-            //    case 6:
-            //        this.Row = 'g';
-            //        break;
-            //    case 7:
-            //        this.Row = 'h';
-            //        break;
-        }
-        private void parseColumnLocation(int i_indexOfColumn)
-        {
-            this.m_Column = (char)(i_indexOfColumn + 17);
-        }
+
+
         public char Row
         {
             get
@@ -102,6 +74,52 @@ namespace B18_Ex02
             }
         }
 
+        public bool IsKing
+        {
+            get
+            {
+                return this.m_isKing;
+            }
+            set
+            {
+                this.m_isKing = value;
+            }
+        }
+
+        private void parseRowLocation(int i_indexOfRow)
+        {
+            this.m_Row = (char)(i_indexOfRow + 49);
+            //switch (i_indexOfRow)
+            //{
+            //    case 0:
+            //        this.Row = 'a';
+            //        break;
+            //    case 1:
+            //        this.Row = 'b';
+            //        break;
+            //    case 2:
+            //        this.Row = 'c';
+            //        break;
+            //    case 3:
+            //        this.Row = 'd';
+            //        break;
+            //    case 4:
+            //        this.Row = 'e';
+            //        break;
+            //    case 5:
+            //        this.Row = 'f';
+            //        break;
+            //    case 6:
+            //        this.Row = 'g';
+            //        break;
+            //    case 7:
+            //        this.Row = 'h';
+            //        break;
+        }
+        private void parseColumnLocation(int i_indexOfColumn)
+        {
+            this.m_Column = (char)(i_indexOfColumn + 17);
+        }
     }
 
 }
