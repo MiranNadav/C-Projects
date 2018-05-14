12009433335a76cd07c21fc8b939aa18c0578c82
Nadav Miran <miran.nadav@gmail.com>
2018-05-14 10:16:40 +0300

    Added VehicleFactory, ruck and Garage classes.

---
+++ B18 Ex03/B18 Ex03/GasTruck.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B18_Ex03
+{
+    class GasTruck
+    {
+    }
+}
