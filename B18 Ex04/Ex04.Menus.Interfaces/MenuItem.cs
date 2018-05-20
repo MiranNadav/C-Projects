6a58ed0d687956bb80050406c281549cb994a8ee
Nadav Miran <miran.nadav@gmail.com>
2018-05-20 18:29:10 +0300

    added classes

---
+++ B18 Ex04/Ex04.Menus.Interfaces/MenuItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Interfaces
+{
+    class MenuItem
+    {
+        private string m_ItemTitle;
+
+        public string ItemTitle
+        {
+            get
+            {
+                return this.m_ItemTitle;
+            }
+            set
+            {
+                this.m_ItemTitle = value;
+            }
+        }
+    }
+}
